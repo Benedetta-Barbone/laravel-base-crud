1c5089889b603a6430cfa0bfb3b38788290d4cce
Benedetta-Barbone <barbone1912@gmail.com>
2024-07-23 17:10:33 +0200

    aggiunti animali nella views

---
+++ resources/views/animals/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('page-title', 'Animal index')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center p-5">
+                <h1>
+                    Animal index
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            @foreach ($animals as $animal )
+                <article class="col-3">
+                    <h2>
+                        {{ $animal->name}}
+                    </h2>
+                    <p>
+                        tipo: {{ $animal->species }}
+                    </p>
+                    <p>
+                        età: {{ $animal->age }} anni
+                    </p>
+                </article>
+
+            @endforeach
+        </div>
+    </div>
+@endsection
